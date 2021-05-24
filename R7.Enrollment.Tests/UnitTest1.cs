45ecc2aefc5ac507804ab10df47b5a5d7740516b
roman-yagodin <roman.yagodin@gmail.com>
2021-05-24 12:07:13 +0300

    Initial commit

---
+++ R7.Enrollment.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace R7.Enrollment.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1 ()
+        {
+        }
+    }
+}
---
+++ R7.Enrollment/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R7.Enrollment
+{
+    public class Class1
+    {
+    }
+}
