ec8bd07bbf6663eb90415e64b0c99ee69908ece3
roman-yagodin <roman.yagodin@gmail.com>
2021-05-26 11:51:52 +0300

    Start building HTML renderer

--- R7.Enrollment.Tests/Program.cs
+++ R7.Enrollment.Tests/Program.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Text;
+using System.Xml;
 using R7.Enrollment.Data;
+using R7.Enrollment.Views;
 
 namespace R7.Enrollment.Tests
 {
@@ -8,6 +12,13 @@ namespace R7.Enrollment.Tests
         {
             var db = new TandemEntrantRatingDb ("./data/sample.xml");
             db.Dump ();
+            
+            var htmlRenderer = new TandemEntrantRatingHtmlRenderer (db);
+            var sb = new StringBuilder ();
+            var html = XmlWriter.Create (sb);
+            htmlRenderer.Render (html);
+            html.Close ();
+            Console.WriteLine (sb.ToString ());
         }
     }
 }
