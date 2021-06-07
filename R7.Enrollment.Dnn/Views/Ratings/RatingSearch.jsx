d0597a305d661cdfb9e8efbf800707c295b55a3e
roman-yagodin <roman.yagodin@gmail.com>
2021-06-07 14:18:27 +0300

    Don't set starting input value

--- R7.Enrollment.Dnn/Views/Ratings/RatingSearch.jsx
+++ R7.Enrollment.Dnn/Views/Ratings/RatingSearch.jsx
@@ -65,7 +65,7 @@ class RatingSearch extends React.Component {
                 </div>
                 <div className="form-group">
                     <label htmlFor="enrRatingSearch_EntrantId">Идентификатор абитуриента</label>
-                    <input type="text" name="personalNumber" className="form-control" id="enrRatingSearch_EntrantId" value="2100021" />
+                    <input type="text" name="personalNumber" className="form-control" id="enrRatingSearch_EntrantId" placeholder="2100021" />
                 </div>
                 <button type="submit" className="btn btn-primary">Показать списки</button>
             </form>
