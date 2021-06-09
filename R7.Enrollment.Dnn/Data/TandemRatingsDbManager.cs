6d4d1a293c5f51c8af05db08b05586f8bdd108f7
roman-yagodin <roman.yagodin@gmail.com>
2021-06-09 15:06:38 +0300

    Reimplement and strengthen database manager logic - will need refactoring

--- R7.Enrollment.Dnn/Data/TandemRatingsDbManager.cs
+++ R7.Enrollment.Dnn/Data/TandemRatingsDbManager.cs
@@ -5,92 +5,136 @@ using System.Text.RegularExpressions;
 using DotNetNuke.Services.FileSystem;
 using R7.Enrollment.Data;
 using R7.Enrollment.Models;
+using SystemFileInfo = System.IO.FileInfo;
 
 namespace R7.Enrollment.Dnn.Data
 {
-    public class DbEntry
+    public class DbSourceFile
+    {
+        public string Name { get; set; }
+        
+        public long Length { get; set; }
+        
+        public DateTime LastWriteTimeUtc { get; set; } 
+    }
+    
+    public class DbSetEntry
     {
         public int PortalId { get; set; }
         
-        public string Key { get; set; }
+        public IList<DbSourceFile> SourceFiles { get; set; }
         
-        public string Path { get; set; }
-        
-        public DateTime LastModified { get; set; }
-        
-        public TandemRatingsDb Db { get; set; }
+        public IList<TandemRatingsDb> Databases { get; set; } = new List<TandemRatingsDb> ();
     }
-
 
     public class TandemRatingsDbManager
     {
+        private const string FolderPath = "enrollment";
+
         private static readonly Lazy<TandemRatingsDbManager> _instance = new Lazy<TandemRatingsDbManager> ();
 
         public static TandemRatingsDbManager Instance => _instance.Value;
 
-        private IList<DbEntry> Databases = new List<DbEntry> ();
+        private IList<DbSetEntry> DbSets = new List<DbSetEntry> ();
 
-        void LoadDatabases (int portalId, string dbFolderPath)
+        bool DbSetIsActual (int portalId)
         {
-            var dbFolder = FolderManager.Instance.GetFolder (portalId, dbFolderPath);
-            if (dbFolder == null) {
-                return;
+            var dbSet = DbSets.FirstOrDefault (dbs => dbs.PortalId == portalId);
+            if (dbSet == null) {
+                return false;
             }
 
-            var dbFiles = FolderManager.Instance.GetFiles (dbFolder)
-                .Where (f => Regex.IsMatch (f.FileName, "^enr_[^.]*\\.xml$"));
+            var srcFiles = GetSourceFiles (portalId).ToList ();
+            if (dbSet.SourceFiles.Count != srcFiles.Count) {
+                return false;
+            }
 
-            foreach (var dbFile in dbFiles) {
-                var db = new TandemRatingsDb ();
-                db.Load (dbFile.PhysicalPath);
-                Databases.Add (new DbEntry {
-                    PortalId = 0,
-                    Path = dbFile.RelativePath,
-                    LastModified = dbFile.LastModificationTime,
-                    Db = db,
-                    Key = db.EntrantRatingEnvironment.CampaignTitle
-                });
+            foreach (var srcFile in srcFiles) {
+                var dbSrcFile = dbSet.SourceFiles.FirstOrDefault (
+                    sf => sf.Name.Equals (srcFile.Name, StringComparison.CurrentCultureIgnoreCase)
+                          && sf.Length == srcFile.Length
+                          && sf.LastWriteTimeUtc == srcFile.LastWriteTimeUtc);
+                if (dbSrcFile == null) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        IEnumerable<SystemFileInfo> GetSourceFiles (int portalId)
+        {
+            var dbFolder = FolderManager.Instance.GetFolder (portalId, FolderPath);
+            if (dbFolder == null) {
+                return Enumerable.Empty<SystemFileInfo> ();
+            }
+
+            return FolderManager.Instance.GetFiles (dbFolder)
+                .Where (f => Regex.IsMatch (f.FileName, "^enr_[^.]*\\.xml$"))
+                .Select (f => new SystemFileInfo (f.PhysicalPath));
+        }
+
+        DbSetEntry GetDbSet (int portalId) => DbSets.FirstOrDefault (dbs => dbs.PortalId == portalId);
+        
+        void EnsureDbSetIsActual (int portalId)
+        {
+            if (!DbSetIsActual (portalId)) {
+                ReloadDbSet (portalId);
             }
         }
 
-        public IList<EntrantRatingEnvironment> GetCampaigns (int portalId)
+        void ReloadDbSet (int portalId)
         {
-            if (Databases.Count == 0) {
-                LoadDatabases (portalId, "enrollment");
+            var dbSet = GetDbSet (portalId);    
+            if (dbSet != null) {
+                DbSets.Remove (dbSet);
             }
-
-            return Databases.Select (dbe => dbe.Db.EntrantRatingEnvironment).ToList ();
+            
+            dbSet = new DbSetEntry {
+                PortalId = portalId
+            };
+            
+            var srcFiles = GetSourceFiles (portalId).ToList ();
+            dbSet.SourceFiles = srcFiles.Select (sf =>
+                new DbSourceFile {
+                    Name = sf.Name,
+                    Length = sf.Length,
+                    LastWriteTimeUtc = sf.LastWriteTimeUtc
+                }
+            ).ToList ();
+                
+            foreach (var srcFile in srcFiles) {
+                var db = new TandemRatingsDb ();
+                db.Load (srcFile.FullName);
+                dbSet.Databases.Add (db);
+            }
+            
+            DbSets.Add (dbSet);
+        }
+        
+        public IEnumerable<EntrantRatingEnvironment> GetCampaigns (int portalId)
+        {
+            lock (this) {
+                EnsureDbSetIsActual (portalId);
+                var dbSet = DbSets.FirstOrDefault (dbs => dbs.PortalId == portalId);
+                if (dbSet == null) {
+                    return Enumerable.Empty<EntrantRatingEnvironment> ();
+                }
+                return dbSet.Databases.Select (dbs => dbs.EntrantRatingEnvironment);
+            }
         }
 
         public TandemRatingsDb GetDb (string campaignTitle, int portalId)
         {
-            if (Databases.Count == 0) {
-                LoadDatabases (portalId, "enrollment");
+            lock (this) {
+                EnsureDbSetIsActual (portalId);
+                var dbSet = DbSets.FirstOrDefault (dbs => dbs.PortalId == portalId);
+                if (dbSet == null) {
+                    return null;
+                }
+                return dbSet.Databases.FirstOrDefault (dbs =>
+                    dbs.EntrantRatingEnvironment.CampaignTitle == campaignTitle);
             }
-            
-            var dbEntry = Databases.FirstOrDefault (dbe => dbe.Key == campaignTitle);
-            if (dbEntry == null) {
-                return null;
-            }
-            
-            var file = FileManager.Instance.GetFile (portalId, dbEntry.Path);
-            if (file == null) {
-                // file was deleted
-                Databases.Remove (dbEntry);
-                return null;
-            }
-
-            if (file.LastModificationTime == dbEntry.LastModified) {
-                // file was not modified
-                return dbEntry.Db;
-            }
-            
-            // file was modified
-            var db = new TandemRatingsDb ();
-            db.Load (file.PhysicalPath);
-            dbEntry.LastModified = file.LastModificationTime;
-            dbEntry.Db = db;
-            return db;
         }
     }
 }
