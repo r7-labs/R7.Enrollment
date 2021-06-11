1366939cb7501d4298131b70584353ca243c838d
roman-yagodin <roman.yagodin@gmail.com>
2021-06-11 12:20:06 +0300

    Get data folder path from DNN portal settings

--- R7.Enrollment.Dnn/Data/TandemRatingsDbManager.cs
+++ R7.Enrollment.Dnn/Data/TandemRatingsDbManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using DotNetNuke.Entities.Portals;
 using DotNetNuke.Services.FileSystem;
 using R7.Enrollment.Data;
 
@@ -9,15 +10,14 @@ namespace R7.Enrollment.Dnn.Data
 {
     public class TandemRatingsDbManager: TandemRatingsDbManagerBase
     {
-        private const string FolderPath = "enrollment";
-
         private static readonly Lazy<TandemRatingsDbManager> _instance = new Lazy<TandemRatingsDbManager> ();
 
         public static TandemRatingsDbManager Instance => _instance.Value;
 
         protected override IEnumerable<System.IO.FileInfo> GetSourceFiles (int portalId)
         {
-            var dbFolder = FolderManager.Instance.GetFolder (portalId, FolderPath);
+            var folderPath = GetDataFolderSetting (portalId);
+            var dbFolder = FolderManager.Instance.GetFolder (portalId, folderPath);
             if (dbFolder == null) {
                 return Enumerable.Empty<System.IO.FileInfo> ();
             }
@@ -25,6 +25,16 @@ namespace R7.Enrollment.Dnn.Data
             return FolderManager.Instance.GetFiles (dbFolder)
                 .Where (f => Regex.IsMatch (f.FileName, "^enr_rating_[^.]*\\.xml$"))
                 .Select (f => new System.IO.FileInfo (f.PhysicalPath));
+        }
+
+        string GetDataFolderSetting (int portalId)
+        {
+            var portalSettings = PortalController.Instance.GetPortalSettings (portalId);
+            portalSettings.TryGetValue ("R7_Enrollment_Ratings__DataFolder", out string dataFolderPath);
+            if (string.IsNullOrEmpty (dataFolderPath)) {
+                return "enrollment";
+            }
+            return dataFolderPath;
         }
     }
 }
