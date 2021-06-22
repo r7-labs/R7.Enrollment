1fdc26eaa81a95eab8aa489e82a93b8f317a8c25
roman-yagodin <roman.yagodin@gmail.com>
2021-06-22 10:37:33 +0300

    Display "No data" message inside database info card

--- R7.Enrollment.Dnn/Views/Ratings/RatingSearch.jsx
+++ R7.Enrollment.Dnn/Views/Ratings/RatingSearch.jsx
@@ -44,20 +44,30 @@ class RatingSearchDbInfo extends React.Component {
     }
 
     render () {
+        return (
+            <div className="card card-body bg-light mb-3">
+                <h5 className="card-title">База данных списков абитуриентов</h5>
+                {this.renderDbInfo ()}
+            </div>
+        );
+    }
+
+    getCampaignItems () {
         const items = [];
         for (let campaign of this.props.campaigns) {
             items.push (<li>{this.formatCampaignTitle (campaign)}</li>);
         }
+        return items;
+    }
+
+    renderDbInfo () {
         if (this.props.campaigns.length > 0) {
             return (
-                <div className="card card-body bg-light mb-3">
-                    <h5 className="card-title">База данных списков абитуриентов</h5>
-                    <ul className="list-unstyled mb-0">{items}</ul>
-                </div>
+                <ul className="list-unstyled mb-0">{this.getCampaignItems ()}</ul>
             );
         }
         return (
-            <p class="alert alert-danger">Не удалось получить данные! Перезагрузите страницу и попробуйте снова.</p>
+            <p class="alert alert-danger mb-0">Не удалось получить данные! Перезагрузите страницу и попробуйте снова.</p>
         );
     }
 }
