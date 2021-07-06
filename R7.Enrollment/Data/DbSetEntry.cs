a46be009e1b5148e0113d44561e09777a6989c02
roman-yagodin <roman.yagodin@gmail.com>
2021-07-06 16:29:42 +0300

    Remove unused prop

--- R7.Enrollment/Data/DbSetEntry.cs
+++ R7.Enrollment/Data/DbSetEntry.cs
@@ -4,10 +4,8 @@ namespace R7.Enrollment.Data
 {
     public class DbSetEntry
     {
-        public int PortalId { get; set; }
-        
         public IList<DbSourceFile> SourceFiles { get; set; }
-        
+
         public IList<TandemRatingsDb> Databases { get; set; } = new List<TandemRatingsDb> ();
     }
 }
