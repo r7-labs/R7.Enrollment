816ba705a6a97611b78fad78ce94c6be66f69f04
roman-yagodin <roman.yagodin@gmail.com>
2021-07-08 16:03:19 +0300

    Output some messages to the console

--- R7.Enrollment.Tests/Program.cs
+++ R7.Enrollment.Tests/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 using System.Xml;
@@ -18,13 +19,23 @@ namespace R7.Enrollment.Tests
 
             Directory.CreateDirectory ("output");
             var dataFiles = Directory.GetFiles ("./data", "enr_rating_*.xml");
+            if (dataFiles.Length == 0) {
+                Console.WriteLine ("No data files found in the \"data\" folder!");
+                return;
+            }
+
             foreach (var dataFile in dataFiles) {
+                Console.Write ($"Processing \"{Path.GetFileName (dataFile)}\" file... ");
                 var db = new TandemRatingsDb ();
                 db.Load (dataFile, consolidate: true);
                 var env = db.EntrantRatingEnvironment;
                 RenderToFile (db, $"./output/{FilenameFromCampaignTitle (env.CampaignTitle)}.html", renderer);
-                RenderToFile (db, $"./output/{FilenameFromCampaignTitle (env.CampaignTitle)}-consolidated.html", consolidatedRenderer);
+                RenderToFile (db, $"./output/{FilenameFromCampaignTitle (env.CampaignTitle)}-consolidated.html",
+                    consolidatedRenderer);
+                Console.WriteLine ("Done!");
             }
+
+            Console.WriteLine ("See results in the \"output\" folder.");
         }
 
         static string FilenameFromCampaignTitle (string campaignTitle)
